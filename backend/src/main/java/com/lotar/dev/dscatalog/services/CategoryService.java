1860354090f29ab6619405657f82132882d0e875
Lotar <amaral.lucas.lotar#gmail.com>
2026-01-09 18:33:00 -0300

    add DTOs and POST new Category

--- backend/src/main/java/com/lotar/dev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/lotar/dev/dscatalog/services/CategoryService.java
@@ -1,11 +1,16 @@
 package com.lotar.dev.dscatalog.services;
 
+import com.lotar.dev.dscatalog.dto.CategoryDTO;
 import com.lotar.dev.dscatalog.entities.Category;
 import com.lotar.dev.dscatalog.repositories.CategoryRepository;
+import com.lotar.dev.dscatalog.services.exeptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -13,8 +18,22 @@ public class CategoryService {
   @Autowired
   private CategoryRepository categoryRepository;
 
-  public List<Category> findAll() {
+  public List<CategoryDTO> findAll() {
+      List<Category> list = categoryRepository.findAll();
+      return list.stream().map(CategoryDTO::new).toList();
+  }
 
-    return categoryRepository.findAll();
+  public CategoryDTO findById(Long id) {
+    Optional<Category> obj = categoryRepository.findById(id);
+    Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+    return new CategoryDTO(entity);
+  }
+
+  @Transactional
+  public CategoryDTO insert(CategoryDTO dto) {
+    Category entity = new Category();
+    entity.setName(dto.getName());
+    entity =  categoryRepository.save(entity);
+    return new CategoryDTO(entity);
   }
 }

df818efa8843fb6e6eec11fe150726a03a4e8cbb
Lotar <amaral.lucas.lotar#gmail.com>
2026-01-19 10:02:01 -0300

    Add Update Category

--- backend/src/main/java/com/lotar/dev/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/lotar/dev/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -1,17 +1,28 @@
 package com.lotar.dev.dscatalog.resources.exceptions;
 
-import com.lotar.dev.dscatalog.services.exeptions.EntityNotFoundException;
+import com.lotar.dev.dscatalog.services.exeptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+    StandardError err = new StandardError();
+    err.setTimestamp(Instant.now());
+    err.setStatus(HttpStatus.NOT_FOUND.value());
+    err.setError("Resource not found");
+    err.setMessage(e.getMessage());
+    err.setPath(request.getRequestURI());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+  }
 
   @ExceptionHandler(EntityNotFoundException.class)
   public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {

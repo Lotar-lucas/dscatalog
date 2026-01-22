25cb75aa222e24aa48f6f551b2b3062794a00c4c
Lotar <amaral.lucas.lotar#gmail.com>
2026-01-21 23:55:20 -0300

    Refactor pageable

--- backend/src/main/java/com/lotar/dev/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/lotar/dev/dscatalog/services/ProductService.java
@@ -10,7 +10,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,8 +27,8 @@ public class ProductService {
   private CategoryRepository categoryRepository;
 
 
-  public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-    Page<Product> list = ProductRepository.findAll(pageRequest);
+  public Page<ProductDTO> findAllPaged(Pageable pageable) {
+    Page<Product> list = ProductRepository.findAll(pageable);
       return list.map(ProductDTO::new);
   }
 

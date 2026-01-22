3c427c8457220d3ea32ad68d9ba32afde5d1e984
Lotar <amaral.lucas.lotar#gmail.com>
2026-01-21 22:45:54 -0300

    Product entity, SQL script

--- backend/src/main/java/com/lotar/dev/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/lotar/dev/dscatalog/resources/CategoryResource.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 
 @RestController
